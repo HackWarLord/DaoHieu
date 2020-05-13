5f3364ae077da64ac3ff87223258a89c2ebdc70e
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2020-05-13 19:20:02 +0000

    chore(package): update eslint to version 2.5.1

--- Crypto/Flows/Screens/Home/ContentView.swift
+++ Crypto/Flows/Screens/Home/ContentView.swift
@@ -15,4 +15,5 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Image("HomeBackgroun")
-                .ignoresSafe
+                .ignoresSafeArea()
+            
