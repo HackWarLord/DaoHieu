39bf45a30e185255ee8f9b313a04032661a46663
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2020-06-02 21:10:27 +0000

    More Changes

--- Crypto/Flows/Screens/Home/ContentView.swift
+++ Crypto/Flows/Screens/Home/ContentView.swift
@@ -26,4 +26,5 @@ struct ContentView: View {
                     Text("To the Crypto")
                         .foregroundColor(.white)
                         .font(.system(size: 35))
-                        
+                        .fontWeight(.bold)
+    
