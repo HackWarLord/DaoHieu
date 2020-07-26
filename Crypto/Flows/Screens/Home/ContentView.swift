5a9bc7b3b7bb1b57bcc74a2b6f7016462008e001
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2020-07-26 12:18:48 +0000

    Delete .editorconfig

--- Crypto/Flows/Screens/Home/ContentView.swift
+++ Crypto/Flows/Screens/Home/ContentView.swift
@@ -51,4 +51,4 @@ struct ContentView: View {
             }
             .opacity(animation ? 1.0 : 0.0)
             .onAppear() {
-                withAnimation(Animation.eas
+                withAnimation(Animation.easeInOut(duration: 2
