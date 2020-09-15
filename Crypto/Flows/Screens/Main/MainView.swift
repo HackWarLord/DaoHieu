5560758271364a3b21601268efd86b3c05bfe861
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2020-09-15 20:50:18 +0000

    Clang-format

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -33,4 +33,4 @@ struct MainView: View {
     
                                 Text("$21,188.12")
                                     .frame(width: 110, height: 35)
-         
+                              
