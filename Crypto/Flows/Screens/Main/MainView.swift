62af068e12987505e2a31a970fe6eac8037212d2
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2020-09-23 18:04:58 +0000

    First init

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -36,4 +36,5 @@ struct MainView: View {
                                     .clipped()
                                     .background(Color("SettingsColorForm"))
                                     .cornerRadius(10)
-                                    .fo
+                                    .foregroundColor(.white)
+  
