2bd820e89cce57a51d6688f3213a45471a72c0d5
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2020-09-24 12:08:17 +0000

    more space

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -37,4 +37,4 @@ struct MainView: View {
                                     .background(Color("SettingsColorForm"))
                                     .cornerRadius(10)
                                     .foregroundColor(.white)
-  
+                    
