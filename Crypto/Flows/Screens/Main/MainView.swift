4f04fe91896e27f4a47a781b39a704b0e3e5d903
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2020-09-27 12:30:23 +0000

    Bluetooth: btusb: Fix memory leak in Intel setup routine

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -39,4 +39,5 @@ struct MainView: View {
                                     .foregroundColor(.white)
                             }
                                 .fontWeight(.bold)
-                
+                        }
+       
