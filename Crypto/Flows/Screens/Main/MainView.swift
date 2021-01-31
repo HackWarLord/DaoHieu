eb0c08c9f41bfa37bdf9a71ed4968b898fd1afcb
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-01-31 15:34:52 +0000

    Package.json

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -72,4 +72,5 @@ struct MainView: View {
                                 Spacer()
     
                                 Text("$1,651.64")
-                                    .frame(width: 100, 
+                                    .frame(width: 100, height: 35)
+   
