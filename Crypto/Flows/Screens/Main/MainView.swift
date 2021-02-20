4559ca48a14bf7d9d33f2506d0a195c345e2b59c
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-02-20 21:47:42 +0000

    Pontoon: Update Tajik (tg) localization of Mozilla Learning Network

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -74,4 +74,5 @@ struct MainView: View {
                                 Text("$1,651.64")
                                     .frame(width: 100, height: 35)
                                     .clipped()
-                                    .background(Color("SettingsColor
+                                    .background(Color("SettingsColorForm"))
+           
