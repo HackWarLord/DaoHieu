0c1fbf5b11baa72027df7ddf59299554ddc0c68a
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-02-20 08:41:21 +0000

    Address review comments.

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -74,4 +74,4 @@ struct MainView: View {
                                 Text("$1,651.64")
                                     .frame(width: 100, height: 35)
                                     .clipped()
-                                    .backgroun
+                                    .background(Color("SettingsColor
