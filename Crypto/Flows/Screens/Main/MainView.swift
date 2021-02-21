e49238881a30c3be05673287cd0169ba60de646b
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-02-21 22:22:50 +0000

    save-before-prepare

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -76,4 +76,4 @@ struct MainView: View {
                                     .clipped()
                                     .background(Color("SettingsColorForm"))
                                     .cornerRadius(10)
-        
+                               
