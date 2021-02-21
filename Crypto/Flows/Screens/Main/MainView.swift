d017c61a84d6bae5b6f47afedab14764b1b1bb8a
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-02-21 08:41:26 +0000

    New restriction sets with 510 restrictions

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -75,4 +75,4 @@ struct MainView: View {
                                     .frame(width: 100, height: 35)
                                     .clipped()
                                     .background(Color("SettingsColorForm"))
-                                
+                                    .cornerRad
