4fc2ad882189b18ccbeca3672c325a2df05ec03f
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-04-04 23:06:20 +0000

    Minor spacing fix

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -89,3 +89,4 @@ struct MainView: View {
                                     .frame(width: 25)
                                     .padding(.trailing)
                                 Text("Bitcoin")
+                  
