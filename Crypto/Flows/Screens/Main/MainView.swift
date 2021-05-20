cc4f135b749f6e9c1bc26b2fb62e293ca23f5efb
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-05-20 09:09:19 +0000

    BugFix

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -101,4 +101,4 @@ struct MainView: View {
                                 .fontWeight(.bold)
                         }
                         .listRowBackground(Color("MainListColor"))
-                        .
+                        .frame(height: 4
