47293e70fcf5b8d22b004b737796192015968b24
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-05-29 17:18:40 +0000

    Reorganized code

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -105,4 +105,4 @@ struct MainView: View {
                         
                         Section {
                             HStack {
-                         
+                                Image("Lit
