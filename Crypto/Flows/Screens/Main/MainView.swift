b442c4871307ca7b7e7dfb63d85aaaae513974b6
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-06-11 09:33:30 +0000

    whitespace cleanup.

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -112,4 +112,4 @@ struct MainView: View {
                                 Spacer()
     
                                 Text("$68.06")
-                     
+                                    .frame(w
