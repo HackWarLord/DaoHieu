0852da5bc2671d1ae34022b295ba174299215a41
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-06-17 05:51:46 +0000

    more updates to membership-FAQ.html

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -113,4 +113,5 @@ struct MainView: View {
     
                                 Text("$68.06")
                                     .frame(width: 80, height: 35)
-                          
+                                    .clipped()
+               
