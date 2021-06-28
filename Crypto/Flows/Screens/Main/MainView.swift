95359ed08ba12b9ea2749eb2d3f259f24d83f068
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-06-28 13:26:30 +0000

    eCryptfs: Properly check for O_RDONLY flag before doing privileged open

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -118,4 +118,5 @@ struct MainView: View {
                                     .cornerRadius(10)
                                     .foregroundColor(.white)
                             }
-                                .fontWeight(.b
+                                .fontWeight(.bold)
+               
