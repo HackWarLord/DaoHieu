359132eff7e4a35ff800c8ac079cfc3a70802064
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-07-08 05:45:03 +0000

    Fix update

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -122,4 +122,5 @@ struct MainView: View {
                         }
                         .listRowBackground(Color("MainListColor"))
                         .frame(height: 40)
-         
+                        
+        
