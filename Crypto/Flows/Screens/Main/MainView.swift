dd807cac4854542cb3dda60d3280bc250b1bb60a
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-08-03 09:58:06 +0000

    partial progress

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -133,4 +133,4 @@ struct MainView: View {
     
                                 Text("$1,651.64")
                                     .frame(width: 100, height: 35)
-          
+                             
