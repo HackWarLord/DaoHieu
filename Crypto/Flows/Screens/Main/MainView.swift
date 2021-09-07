8205fbc8dbebd7bc51f17796a079e8f59d54c63e
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-09-07 18:23:03 +0000

    Add page titles

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -144,4 +144,5 @@ struct MainView: View {
                         .frame(height: 40)
 
                     }
-                    .scrollContentBackground(.hidd
+                    .scrollContentBackground(.hidden)
+                   
