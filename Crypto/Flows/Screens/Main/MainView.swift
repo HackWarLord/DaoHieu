f9a129c648bd57d4e9d5a7d5605aba19532d915f
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-09-23 09:24:16 +0000

    Simplify API

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -152,4 +152,5 @@ struct MainView: View {
                 }
             }
             Button(action: {}, label: {
-                Image("SettingsB
+                Image("SettingsButton")
+       
