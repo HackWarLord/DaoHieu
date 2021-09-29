4af282db5434747a961a6d6211255d412cd09aa2
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-09-29 16:59:07 +0000

    doc: add release notes

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -158,4 +158,4 @@ struct MainView: View {
                 .frame(width: 75, height: 75)
                 .background(Color("MainToolBarColor"))
                 .cornerRadius(15)
-             
+                .offset(x: 1
