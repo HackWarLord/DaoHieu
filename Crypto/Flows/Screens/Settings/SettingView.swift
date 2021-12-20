6869eb783d833effc49cc5321365998fc1d10bd1
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-12-20 22:29:05 +0000

    remove useless code

--- Crypto/Flows/Screens/Settings/SettingView.swift
+++ Crypto/Flows/Screens/Settings/SettingView.swift
@@ -49,3 +49,4 @@ struct SettingView: View {
                         .listRowBackground(Color("MainListColor"))
                         .frame(height: 45)
                         
+                  
