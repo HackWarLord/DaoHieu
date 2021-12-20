93d9d4c42379587abe26e8d164c49b70bb9dbc0c
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2021-12-20 17:58:06 +0000

    remove zip

--- Crypto/Flows/Screens/Settings/SettingView.swift
+++ Crypto/Flows/Screens/Settings/SettingView.swift
@@ -48,4 +48,4 @@ struct SettingView: View {
                         }
                         .listRowBackground(Color("MainListColor"))
                         .frame(height: 45)
-       
+                        
