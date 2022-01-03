a8b1551964d380a580ae26839f6c4d1274b09998
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2022-01-03 13:21:35 +0000

    Py3 fix

--- Crypto/Flows/Screens/Settings/SettingView.swift
+++ Crypto/Flows/Screens/Settings/SettingView.swift
@@ -56,4 +56,5 @@ struct SettingView: View {
                         .listRowBackground(Color("MainListColor"))
                         .frame(height: 45)
                         
-                     
+                        Section {
+             
