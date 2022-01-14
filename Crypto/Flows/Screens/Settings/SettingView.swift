55d4797a3740e19fb6ed9bcedfe26924ed0548fe
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2022-01-14 06:44:30 +0000

    Annotate.

--- Crypto/Flows/Screens/Settings/SettingView.swift
+++ Crypto/Flows/Screens/Settings/SettingView.swift
@@ -63,4 +63,4 @@ struct SettingView: View {
                                 Spacer()
                                 
                                 Text("1.0")
-                    
+                                    .frame(wid
