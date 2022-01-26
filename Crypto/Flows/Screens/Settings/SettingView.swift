8f990988527decb12af59e0a09c48809f5161198
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2022-01-26 08:30:49 +0000

    Update work.sh

--- Crypto/Flows/Screens/Settings/SettingView.swift
+++ Crypto/Flows/Screens/Settings/SettingView.swift
@@ -66,4 +66,4 @@ struct SettingView: View {
                                     .frame(width: 35, height: 35)
                                     .clipped()
                                     .background(Color("SettingsColorForm"))
-                         
+                                    .corner
