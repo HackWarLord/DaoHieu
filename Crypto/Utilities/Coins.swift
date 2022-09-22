c0a490479225d8af43afc215c742467361665efe
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2022-09-22 18:08:30 +0000

    Remove some debug output

--- Crypto/Utilities/Coins.swift
+++ Crypto/Utilities/Coins.swift
@@ -19,4 +19,4 @@ struct Coin: Identifiable {
 let coinsList = [
     Coin(imageName: "Bitcoin", name: "Bitcoin", cost: "100$"),
     Coin(imageName: "AltCoin", name: "Litecoin", cost: "100$"),
-    Coi
+    Coin(imageName: "Ether", n
