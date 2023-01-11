eaf8cffe80e30a696b610caa8f61e7e3ebb55194
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2023-01-11 14:14:52 +0000

    remove trailing space

--- CryptoTests/CryptoTests.swift
+++ CryptoTests/CryptoTests.swift
@@ -11,4 +11,7 @@ import XCTest
 final class CryptoTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the clas
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override fun
