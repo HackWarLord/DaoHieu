ddfcce236ef1890b67c2a8d08d5425e73add0194
HackWarLord <109089261+HackWarLord@users.noreply.github.com>
2023-02-04 22:09:59 +0000

    fix compile issue

--- CryptoTests/CryptoTests.swift
+++ CryptoTests/CryptoTests.swift
@@ -21,4 +21,4 @@ final class CryptoTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any te
+        // Any test you write for XCTest can be a
